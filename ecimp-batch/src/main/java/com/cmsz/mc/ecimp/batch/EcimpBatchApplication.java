bb133c3a5785a0ba0bab7bb92500b0376dbccfb2
Work <Work@zhaozb01>
2017-08-07 11:44:38 +0800

    空的工程

---
+++ ecimp-batch/src/main/java/com/cmsz/mc/ecimp/batch/EcimpBatchApplication.java
@@ -0,0 +1,12 @@
+package com.cmsz.mc.ecimp.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcimpBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcimpBatchApplication.class, args);
+	}
+}
